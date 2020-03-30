02554a127494a544b994ab0d5a5dc39489ad0ff4
Brian Roxbury <roxburybrian@gmail.com>
2020-03-30 10:05:00 -0600

    Initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const audit = {
 
     // What is the value of `this` when we call fn()?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
 
     return result;
   },
@@ -31,7 +31,7 @@ const audit = {
 
     // What is the value of `this` when we call data.verify())?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'data';
 
     return result;
   },
@@ -40,7 +40,7 @@ const audit = {
 
 
 
-  
+
 
 
   exerciseC() {
@@ -54,7 +54,7 @@ const audit = {
 
     // What is the value of `this` when we call dog.bark();
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
 
     return result;
   },
@@ -77,23 +77,26 @@ const audit = {
         let greeting = 'hey';
       }
 
-      // Log A: greeting
+      // Log A: greeting 'hi'
       const newGreeting = ()  => {
         greeting = 'hi';
 
-        // Log B: greeting
+        // Log B: 'hi'
       };
 
       newGreeting();
 
-      // Log C: greeting
+      // Log C: greeting 'hi'
     };
 
-    // Log D: greeting
+    // Log D: greeting //'welcome'
     greetingGenerator();
 
-    const result = [{
-      // 'ADD YOUR RESULT HERE';
+    const result = [
+      {A: 'hi'},
+      {B: 'hi'},
+      {C: 'hi'},
+      {D: 'welcome'};
     }];
 
     return result;
@@ -109,22 +112,22 @@ const audit = {
   exerciseE() {
 
     var shoe = 'heel';
-    // Log A: 
+    // Log A: 'heel'
 
     function putOnShoe() {
       shoe = 'boot';
 
-      // Log B: shoe
+      // Log B: 'boot'
     }
 
-    // Log C: shoe
+    // Log C: 'heel'
     putOnShoe();
 
-    // Log D: shoe
+    // Log D: shoe 'boot'
 
-    const result = [{
-      // 'ADD YOUR RESULT HERE';
-    }];
+    const result = [
+      {A: 'heel'}, {B: 'boot'}, {C: 'heel'}, {D: 'boot'}
+    ];
 
     return result;
   },
@@ -174,7 +177,9 @@ const audit = {
     // e.g.
     // ['Beckon', 'El Five', 'ChoLon', 'Super Mega Bien']
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = restaurants.map(restaurant => {
+      return restaurant.name
+    })
 
     return result;
   },
@@ -220,7 +225,7 @@ const audit = {
     ];
 
     // Return an array of objects that include just the name of the restaurant and whether takeout is available
-    
+
     // e.g.
     // [
     //    { restaurant: 'Beckon', takeOut: false },
@@ -228,7 +233,14 @@ const audit = {
     //    ..etc
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = restaurants.map(restaurant => {
+      let canTakeOut = {};
+        canTakeOut['restaurant'] = restaurant.name;
+        canTakeOut['takeOut'] = restaurant.takeout;
+
+        return canTakeOut
+
+    });
 
     return result;
   },
@@ -282,14 +294,22 @@ const audit = {
     //    'Asian Fusion': ['ChoLon']
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = restaurants.reduce((newObj, restaurant) => {
+        if (!newObj[restaurant.cuisine]) {
+          newObj[restaurant.cuisine] = [];
+          newObj[restaurant.cuisine].push(restaurant.name)
+        } else {
+          newObj[restaurant.cuisine].push(restaurant.name)
+      };
+      return newObj
+    },{})
 
     return result;
   },
 
   exerciseBonus() {
 
-    // There is a war and nobody knows - the alphabet war! 
+    // There is a war and nobody knows - the alphabet war!
     // There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
 
     // Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return 'Left side wins!', when the right side wins return 'Right side wins!', in other case return 'Let's fight again!'.
@@ -310,7 +330,7 @@ const audit = {
     // All other letters don't have power and are only victims.
 
     // alphabetWar("z");        //=> Right side wins!
-    // alphabetWar("zdqmwpbs"); //=> Let's fight again!
+    // alphabetWar("zdqmwpbs"); //=> Let's fight again! rightside: 1234 leftside: 4
     // alphabetWar("zzzzs");    //=> Right side wins!
     // alphabetWar("wwwwwwz");  //=> Left side wins!
 
@@ -318,8 +338,50 @@ const audit = {
     // Fill out functionality below. Your result variable (line 320) is already set up and does not need to be modified
 
     function alphabetWar(fight) {
+    let leftSide = 0;
+    let rightSide = 0;
 
-    }
+    let array = fight.split('').forEach(character => {
+     switch (character) {
+       case 'w':
+       leftSide += 4;
+       break;
+       case 'p':
+       leftSide += 3;
+       break;
+       case 'b':
+       leftSide += 2;
+       break;
+       case 's':
+       leftSide += 1;
+       break;
+       case 'm':
+       rightSide += 4;
+       break;
+       case 'q':
+       rightSide += 3;
+       break;
+       case 'd':
+       rightSide += 2;
+       break;
+       case 'z':
+       rightSide += 1;
+       break;
+     }
+   });
+
+   if(rightSide > leftSide) {
+     return "Right side wins"
+   }
+
+   if(leftSide > rightSide) {
+     return "Left side wins!"
+   }
+
+   if(leftSide === rightSide) {
+     return "Let's fight again!"
+   }
+ }
 
 
     const result = {
@@ -329,7 +391,7 @@ const audit = {
       four: alphabetWar('wwwwwwz')
     };
 
-    return result;    
+    return result;
   }
 };
 
